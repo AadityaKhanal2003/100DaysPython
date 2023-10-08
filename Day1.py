6160969b748783278c6ef759f281cbe3fb37626e
AadityaKhanal <142284398+AadityaKhanal2003@users.noreply.github.com>
2023-10-07 22:58:27 -0400

    Create Day1.py
    Day 1 completed.

---
+++ Day1.py
@@ -0,0 +1,4 @@
+print(" Welcome to the band generator! ")
+name_city = input("Enter the city you grew up in: ")
+name_pet = input("Enter the name of your pet: ")
+print("Your band name is " + name_city + " " + name_pet)
